d0e8f8abe4ae79d4ca1121d3ee1756c7553527d2
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-04-25 18:47:58 +0200

    Add a new car to the table and clear the dialog

--- src/main/java/hellojetty/TableView.java
+++ src/main/java/hellojetty/TableView.java
@@ -1,27 +1,34 @@
 package hellojetty;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean(eager = true)
-public class TableView {
+@SessionScoped
+public class TableView implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	private String brand;
 	private String model;
 	
-	private List<Car> cars = Arrays.asList(
-			new Car("Audi", "A4"),
-			new Car("BMW", "3 series"),
-			new Car("Citroen", "C5"),
-			new Car("Dacia", "Dokker"),
-			new Car("Fiat", "Tipo")
-		);
+	private List<Car> cars = new ArrayList<>(
+			Arrays.asList(
+					new Car("Audi", "A4"),
+					new Car("BMW", "3 series"),
+					new Car("Citroen", "C5"),
+					new Car("Dacia", "Dokker"),
+					new Car("Fiat", "Tipo")
+			));
 	
 	public void addCar() {
 		Car car = new Car(brand, model);
 		cars.add(car);
+		clearForm();
 	}
 
 	public List<Car> getCars() {
@@ -46,6 +53,11 @@ public class TableView {
 
 	public void setModel(String model) {
 		this.model = model;
+	}
+	
+	private void clearForm() {
+		brand = "";
+		model = "";
 	}
 	
 }
